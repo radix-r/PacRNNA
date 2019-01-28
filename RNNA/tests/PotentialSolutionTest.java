aee7729400e06673db388cedd9099c6ce514f9c3
rosscwagner <rosscwagner@gmail.com>
2019-01-28 13:23:57 -0500

    Makes food array

--- RNNA/tests/PotentialSolutionTest.java
+++ RNNA/tests/PotentialSolutionTest.java
@@ -4,3 +4,13 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+public class PotentialSolutionTest{
+    PotentialSolution psTester;
+
+
+    @BeforeClass
+    public void testSetup(){
+        psTester = new PotentialSolution();
+    }
+
+}
